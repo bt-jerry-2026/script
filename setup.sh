1a3c75cb730c1ea295d94c2fbd97284437f5a3b4
BT Jerry <bt.jerry.2026@gmail.com>
2026-04-05 18:57:15 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -122,12 +122,16 @@ server {
     ssl_certificate /etc/letsencrypt/live/${DOMAIN}/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/${DOMAIN}/privkey.pem;
 
+    location = /openclaw {
+        return 301 /openclaw/;
+    }
+
     location /openclaw/ {
         proxy_pass http://127.0.0.1:18789;
         proxy_http_version 1.1;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-For \$remote_addr;
         proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection \$connection_upgrade;
