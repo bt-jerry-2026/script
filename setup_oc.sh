5f5081d72ce815cfa98a63aa5a31e9d077be5044
BT Jerry <bt.jerry.2026@gmail.com>
2026-04-05 18:53:01 +0800

    Update setup_oc.sh

--- setup_oc.sh
+++ setup_oc.sh
@@ -1,8 +1,10 @@
 openclaw config set gateway.mode '"local"'
 openclaw config set gateway.bind '"loopback"'
 openclaw config set gateway.port 18789
+openclaw config set gateway.trustedProxies '["127.0.0.1", "::1"]'
 openclaw config set gateway.controlUi.enabled true
 openclaw config set gateway.controlUi.basePath '"/openclaw"'
 openclaw config set gateway.controlUi.allowedOrigins '["https://xshuliner.online"]'
+
 
 openclaw gateway restart
